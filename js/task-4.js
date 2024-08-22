304e3c48ee7f65b0f475219bb1deb053df326177
Oleh Shyian <shiyan4242@gmail.com>
2024-08-22 16:13:25 +0200

    all tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+const handleFormSubmit = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.elements.email;
+    const password = form.elements.password;
+
+    if (!email.value || !password.value) {
+        alert('All form fields must be filled in');
+        return; 
+    }
+
+    const data = {
+        formEmail: email.value,
+        formPass: password.value,
+    };
+    console.log('data:', data);
+
+    form.reset();
+};
+
+form.addEventListener('submit', handleFormSubmit);
